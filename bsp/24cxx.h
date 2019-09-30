dd70820b8bb005fc1433c2976d49128958dfe880
whyeneo <whye-001@163.com>
2019-09-30 09:23:15 +0800

    重新编译

--- bsp/24cxx.h
+++ bsp/24cxx.h
@@ -2,18 +2,7 @@
 #define _24CXX_H
 #include "sys.h"
 #include "myiic.h"
-//////////////////////////////////////////////////////////////////////////////////	 
-//本程序只供学习使用，未经作者许可，不得用于其它任何用途
-//ALIENTEK STM32F103开发板
-//SDRAM驱动代码	   
-//正点原子@ALIENTEK
-//技术论坛:www.openedv.com
-//创建日期:2017/5/30
-//版本：V1.0
-//版权所有，盗版必究。
-//Copyright(C) 广州市星翼电子科技有限公司 2014-2024
-//All rights reserved									  
-////////////////////////////////////////////////////////////////////////////////// 	
+
 
 #define AT24C01		127
 #define AT24C02		255
@@ -24,7 +13,7 @@
 #define AT24C64	    8191
 #define AT24C128	16383
 #define AT24C256	32767  
-//STM32F429开发板使用的是24c02，所以定义EE_TYPE为AT24C02
+//STM32F103开发板使用的是24c02，所以定义EE_TYPE为AT24C02
 #define EE_TYPE AT24C02
 					  
 uint8_t AT24CXX_ReadOneByte(uint16_t ReadAddr);							//指定地址读取一个字节
